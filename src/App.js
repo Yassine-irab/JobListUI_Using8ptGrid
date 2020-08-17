5b7f1676b8ef9bb9f20378aa612c21a58c09948b
yassine irab <yassineirab.contact@gmail.com>
2020-08-17 15:16:15 +0100

    Finishing the prooject - adding new style and fetching data with the array method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+//Components
+import Job from "./components/job";
+//Data
+import { jobsAvailable } from "./data/data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="container">
+        <div className="job-container">
+          {
+            jobsAvailable.map((job, index) => (
+              <Job
+              key={index}
+              Company={job.company}
+              Title={job.title}
+              Location={job.location}
+              DatePosted={job.datePosted}/>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }
